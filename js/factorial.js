9a1dd37ce425c4f92ea06e59326723253f7b9988
interdimensionalpizza <michaellake05@gmail.com>
2023-06-07 16:55:50 -0700

    finished challenge

--- js/factorial.js
+++ js/factorial.js
@@ -1,2 +1,7 @@
-exports.factorial = function(num) {
+factorial = function(num) {
+    if (num == 1) {
+        return 1
+    }
+    return num * factorial(num - 1)
 };
+console.log(factorial(4)) //24
